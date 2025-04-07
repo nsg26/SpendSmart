44469b52b02dec756e3ea54306e80a51501f3aa2
Nitish Sarkar <nitish.sarkar@gmail.com>
2025-04-07 23:01:56 +0100

    Add project files.

---
+++ SpendSmart/Models/Expense.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Security.Principal;
+
+namespace SpendSmart.Models
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+        public decimal Value { get; set; }
+
+        [Required]
+        public string? Description { get; set; }
+    }
+}
---
+++ SpendSmart/Models/SpendSmartDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SpendSmart.Models
+{
+    public class SpendSmartDBContext:DbContext
+
+    {
+        public DbSet<Expense> Expenses { get; set; }
+        public SpendSmartDBContext(DbContextOptions<SpendSmartDBContext> options)
+            :base(options) 
+        {
+            
+        }
+    }
+}
